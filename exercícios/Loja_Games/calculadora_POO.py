6343b0c0862ce7c6050ce2b584925fd8c9149faa
marcos-vnicius <mv753210@gmail.com>
2026-05-24 19:30:24 -0300

    exercícios

---
+++ exercícios/Loja_Games/calculadora_POO.py
@@ -0,0 +1,59 @@
+class Calculadora():
+    def __init__(self, num_1, num_2):
+        self.number_1 = num_1
+        self.number_2 = num_2
+    
+    def somar(self):
+        resultado = self.number_1 + self.number_2
+        return resultado
+    
+    def subtrair(self):
+        resultado = self.number_1 - self.number_2
+        return resultado
+    
+    def multiplicar(self):
+        resultado = self.number_1 * self.number_2
+        return resultado
+    
+    def dividir(self):
+        resultado = self.number_1 / self.number_2
+        return resultado
+    
+def main():
+    while True:
+        try:
+            print("----- CALCULADORA PYTHON -----\n")
+            primeiro_valor = int(input("Informe o primeiro valor: "))
+            segundo_valor = int(input("Informe o segundo valor: "))
+            opcao = int(input("\nEscolha uma opção:\n [1 - Somar] [2 - Subtrair] [3 - Multiplicar] [4 - Dividir]\n -> "))
+            calculadora_python = Calculadora(primeiro_valor, segundo_valor)
+
+            if opcao == 1:
+                print(f"\nO resultado da soma é igual a {calculadora_python.somar()}")
+                
+            elif opcao == 2:
+                print(f"\nO resultado da subtração é igual a {calculadora_python.subtrair()}")
+                
+            elif opcao == 3:
+                print(f"\nO resultado da multiplicação é igual a {calculadora_python.multiplicar()}")
+                
+            elif opcao == 4:
+                print(f"\nO resultado da dvisão é igual a {calculadora_python.dividir()}")
+
+            else:
+                mensagem = f"[Operação Inválida]"
+                return mensagem
+            
+            opcao_2 = input("Fazer outra operação? [1 - Sim] ou [Qualquer tecla para encerrar] -> ")
+            if opcao_2 in ["1", "sim", "s"]:
+                continue
+            else:
+                print("[Programa Encerrado]\n")
+                break
+
+            
+        except ValueError:
+            print("\n[Operação Inválida]")
+            continue
+            
+main()
