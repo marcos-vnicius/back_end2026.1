0ea036e9dbffe80735efbb9e069af5229b7e8713
marcos-vnicius <mv753210@gmail.com>
2026-05-29 22:29:40 -0300

    Desafio Django

---
+++ Desafio Django/Django/cadastro/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def home(request):
+    titulo = "Página Inicial"
+    return render(request,'cadastros/home.html', {'titulo_home':titulo})
+
+def tabela_alunos(request):
+    titulo = "Alunos Cadastrados"
+    lista_alunos = [
+        {'nome':'Marcos', 'curso':'Python', 'turma':'2026.1'},
+        {'nome':'Anna', 'curso':'Django', 'turma':'2026.2'},
+        {'nome':'Carlos', 'curso':'JavaScript', 'turma':'2026.3'},
+    ]
+
+    return render(request, 'cadastros/alunos.html', {'list_alunos':lista_alunos, 'msg':titulo} )
